e3005a4edd5d0a7c331662d4561262263df5c195
Adam Morrissett <me@adamlm.com>
2020-03-28 17:57:33 -0400

    Move Python rigid transform test code to separate file
    - Add line to optionally make the A matrix a numpy array.
      It can be replace with the original numpy matrix version if needed

---
+++ test_rigid_transform_3D.py
@@ -0,0 +1,67 @@
+from numpy import *
+from math import sqrt
+
+from rigid_transform_3D import rigid_transform_3D
+
+# Test with random data
+
+# Random rotation and translation
+R = mat(random.rand(3,3))
+t = mat(random.rand(3,1))
+
+# make R a proper rotation matrix, force orthonormal
+U, S, Vt = linalg.svd(R)
+R = U*Vt
+
+# remove reflection
+if linalg.det(R) < 0:
+   Vt[2,:] *= -1
+   R = U*Vt
+
+# number of points
+n = 10
+
+# A = mat(random.rand(3, n));
+A = array(random.rand(3, n));
+B = R*A + tile(t, (1, n))
+
+# Recover R and t
+ret_R, ret_t = rigid_transform_3D(A, B)
+
+# Compare the recovered R and t with the original
+B2 = (ret_R*A) + tile(ret_t, (1, n))
+
+# Find the root mean squared error
+err = B2 - B
+err = multiply(err, err)
+err = sum(err)
+rmse = sqrt(err/n);
+
+print("Points A")
+print(A)
+print("")
+
+print("Points B")
+print(B)
+print("")
+
+print("Ground truth rotation")
+print(R)
+
+print("Recovered rotation")
+print(ret_R)
+print("")
+
+print("Ground truth translation")
+print(t)
+
+print("Recovered translation")
+print(ret_t)
+print("")
+
+print("RMSE:", rmse)
+
+if rmse < 1e-5:
+    print("Everything looks good!\n");
+else:
+    print("Hmm something doesn't look right ...\n");
