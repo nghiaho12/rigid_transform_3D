9312428dc62301a1a1795be16aa9bcbabda0bc21
Nghia Ho <nghiaho12@yahoo.com>
2020-10-04 12:48:13 -0700

    Refactor to numpy best practices. Using numpy array instead of numpy matrix.

--- rigid_transform_3D.py
+++ rigid_transform_3D.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from numpy import *
+import numpy as np
 from math import sqrt
 
 # Input: expects 3xN matrix of points
@@ -9,46 +9,44 @@ from math import sqrt
 # t = 3x1 column vector
 
 def rigid_transform_3D(A, B):
-    assert len(A) == len(B)
+    assert A.shape == B.shape
 
-    num_rows, num_cols = A.shape;
-
+    num_rows, num_cols = A.shape
     if num_rows != 3:
-        raise Exception("matrix A is not 3xN, it is {}x{}".format(num_rows, num_cols))
+        raise Exception(f"matrix A is not 3xN, it is {num_rows}x{num_cols}")
 
-    [num_rows, num_cols] = B.shape;
+    num_rows, num_cols = B.shape
     if num_rows != 3:
-        raise Exception("matrix B is not 3xN, it is {}x{}".format(num_rows, num_cols))
+        raise Exception(f"matrix B is not 3xN, it is {num_rows}x{num_cols}")
 
     # find mean column wise
-    centroid_A = mean(A, axis=1)
-    centroid_B = mean(B, axis=1)
+    centroid_A = np.mean(A, axis=1)
+    centroid_B = np.mean(B, axis=1)
 
-    # ensure centroids are 3x1 (necessary when A or B are 
-    # numpy arrays instead of numpy matrices)
+    # ensure centroids are 3x1
     centroid_A = centroid_A.reshape(-1, 1)
     centroid_B = centroid_B.reshape(-1, 1)
 
     # subtract mean
-    Am = A - tile(centroid_A, (1, num_cols))
-    Bm = B - tile(centroid_B, (1, num_cols))
+    Am = A - centroid_A
+    Bm = B - centroid_B
 
-    H = Am * transpose(Bm)
+    H = Am @ np.transpose(Bm)
 
     # sanity check
     #if linalg.matrix_rank(H) < 3:
     #    raise ValueError("rank of H = {}, expecting 3".format(linalg.matrix_rank(H)))
 
     # find rotation
-    U, S, Vt = linalg.svd(H)
-    R = Vt.T * U.T
+    U, S, Vt = np.linalg.svd(H)
+    R = Vt.T @ U.T
 
     # special reflection case
-    if linalg.det(R) < 0:
+    if np.linalg.det(R) < 0:
         print("det(R) < R, reflection detected!, correcting for it ...\n");
         Vt[2,:] *= -1
-        R = Vt.T * U.T
+        R = Vt.T @ U.T
 
-    t = -R*centroid_A + centroid_B
+    t = -R@centroid_A + centroid_B
 
     return R, t
--- test_rigid_transform_3D.py
+++ test_rigid_transform_3D.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
 
-from numpy import *
-from math import sqrt
-
+import numpy as np
 from rigid_transform_3D import rigid_transform_3D
 
 # Test with random data
 
 # Random rotation and translation
-R = mat(random.rand(3,3))
-t = mat(random.rand(3,1))
+R = np.random.rand(3,3)
+t = np.random.rand(3,1)
 
 # make R a proper rotation matrix, force orthonormal
-U, S, Vt = linalg.svd(R)
-R = U*Vt
+U, S, Vt = np.linalg.svd(R)
+R = U@Vt
 
 # remove reflection
-if linalg.det(R) < 0:
+if np.linalg.det(R) < 0:
    Vt[2,:] *= -1
    R = U*Vt
 
 # number of points
 n = 10
 
-# A = mat(random.rand(3, n));
-A = array(random.rand(3, n));
-B = R*A + tile(t, (1, n))
+A = np.random.rand(3, n)
+B = R@A + t
 
 # Recover R and t
 ret_R, ret_t = rigid_transform_3D(A, B)
 
 # Compare the recovered R and t with the original
-B2 = (ret_R*A) + tile(ret_t, (1, n))
+B2 = (ret_R@A) + ret_t
 
 # Find the root mean squared error
 err = B2 - B
-err = multiply(err, err)
-err = sum(err)
-rmse = sqrt(err/n);
+err = err * err
+err = np.sum(err)
+rmse = np.sqrt(err/n);
 
 print("Points A")
 print(A)
